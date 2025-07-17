9805254fcfc29b0a662b5ac49bb0df43f155c46d
Lorenzo <asocialinfroverf@gmail.com>
2025-07-17 16:24:11 -0500

    ADD base.html template

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
   user_profile = {'username': 'Iddy'}
-  return render_template('index.html', title='Home', user=user_profile)
+  posts = [
+    {
+      'author': {'username': 'Oshoke'},
+      'body': 'The quick brown fox jumps over the lazy dog'
+    },
+    {
+      'author': {'username': 'Bubu'},
+      'body': 'Lorem ipsum sit dolor amet'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user_profile, posts=posts)
