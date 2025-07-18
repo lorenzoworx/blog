5bf4611f6d4db401bfc38613dd55606c5f1a0d80
Lorenzo <asocialinfroverf@gmail.com>
2025-07-18 08:56:39 -0500

    APPLY database changes

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
   id: so.Mapped[int] = so.mapped_column(primary_key=True)
   username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
   email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
-  pasword_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+  password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
   def __repr__(self):
     return '<User {}>'.format(self.username)
