86c6e28242e8f952e6bc017ab2c06e66e267f270
Lorenzo <asocialinfroverf@gmail.com>
2025-07-18 21:24:24 -0500

    IMPLEMENT new user registration

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from urllib.parse import urlsplit
+
 
 @app.route('/')
 @app.route('/index')
@@ -22,11 +23,11 @@ def index():
   ]
   return render_template('index.html', title='Home', posts=posts)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if current_user.is_authenticated:
     return redirect(url_for('index'))
-  
   form = LoginForm()
   if form.validate_on_submit():
     user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
@@ -40,7 +41,24 @@ def login():
     return redirect(next_page) 
   return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
   logout_user()
   return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email = form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login'))
+  return render_template('register.html', title='Registration', form=form)
+
