5111c9423eefb5e9ab888ba47f144b452ed3b6bb
Lorenzo <asocialinfroverf@gmail.com>
2025-07-19 01:27:31 -0500

    ADD logic for updating last seen

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,14 @@ from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
+
+
+@app.before_request
+def before_request():
+  if current_user.is_authenticated:
+    current_user.last_seen = datetime.now(timezone.utc)
+    db.session.commit()
 
 
 @app.route('/')
