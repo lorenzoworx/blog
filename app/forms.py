95bcb1682bbca640e0589efbe0b0ecafeef6f45b
Lorenzo <asocialinfroverf@gmail.com>
2025-07-19 10:51:24 -0500

    FIX duplicate username bug found in edit_profile form

--- app/forms.py
+++ app/forms.py
@@ -35,3 +35,13 @@ class EditProfileForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
   bio = TextAreaField('Bio', validators=[Length(min=0, max=140)])
   submit = SubmitField('Submit')
+
+  def __init__(self, original_username, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.original_username = original_username
+
+  def validate_username(self, username):
+    if username.data != self.original_username:
+      user = db.session.scalar(sa.select(User).where(User.username == username.data))
+      if not(user):
+        raise ValidationError('Apologies, that username is already in use. Please use a different username')
