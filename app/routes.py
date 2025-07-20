79da60aa2e3178c7fc945db0c353b4e849987035
Lorenzo <asocialinfroverf@gmail.com>
2025-07-20 10:05:08 -0500

    PAGINATION of posts

--- app/routes.py
+++ app/routes.py
@@ -27,8 +27,9 @@ def index():
     flash('Thoughts shared successfully')
     return redirect(url_for('index'))
 
-  posts = db.session.scalars(current_user.following_posts()).all()
-  return render_template('index.html', title='Home', posts=posts, form=form)
+  page = request.args.get('page', 1, type=int)
+  posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  return render_template('index.html', title='Home', posts=posts.items, form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -141,8 +142,9 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+  page = request.args.get('page', 1, type=int)
   query = sa.select(Post).order_by(Post.timestamp.desc())
-  posts = db.session.scalars(query).all()
-  return render_template('index.html', title='Explore', posts=posts)
+  posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  return render_template('index.html', title='Explore', posts=posts.items)
 
   
--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config:
   MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
   ADMINS = ['asocialinfroverf@gmail.com']
+  POSTS_PER_PAGE = 3
