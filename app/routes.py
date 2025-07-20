d9b440c77fad8ec2dbe0149a373d8c0b411a9aab
Lorenzo <asocialinfroverf@gmail.com>
2025-07-20 10:22:53 -0500

    ADD links for next and previous pages on home and explore page

--- app/routes.py
+++ app/routes.py
@@ -29,7 +29,9 @@ def index():
 
   page = request.args.get('page', 1, type=int)
   posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-  return render_template('index.html', title='Home', posts=posts.items, form=form)
+  next_page = url_for('index', page=posts.next_num) if posts.has_next else None
+  prev_page = url_for('index', page=posts.prev_num) if posts.has_prev else None
+  return render_template('index.html', title='Home', posts=posts.items, form=form, next_page=next_page, prev_page=prev_page)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -145,6 +147,8 @@ def explore():
   page = request.args.get('page', 1, type=int)
   query = sa.select(Post).order_by(Post.timestamp.desc())
   posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-  return render_template('index.html', title='Explore', posts=posts.items)
+  next_page = url_for('explore', page=posts.next_num) if posts.has_next else None
+  prev_page = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+  return render_template('index.html', title='Explore', posts=posts.items, next_page=next_page, prev_page=prev_page)
 
   
