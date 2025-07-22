8591d1a8fbb77ff718505907fc8f7604831f37e9
Lorenzo <asocialinfroverf@gmail.com>
2025-07-22 05:39:49 -0500

    IMPLEMENT user password reset

--- app/forms.py
+++ app/forms.py
@@ -54,3 +54,14 @@ class EmptyForm(FlaskForm):
 class PostForm(FlaskForm):
   post = TextAreaField("What's on your mind?", validators=[DataRequired(), Length(min=1, max=140)])
   submit = SubmitField('Submit')
+
+
+class ResetPasswordRequestForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+  password = PasswordField('Password', validators=[DataRequired()])
+  password_conf = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+  submit = SubmitField('Request Password Reset')
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import db, login, app
+from time import time
+import jwt
 from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -84,6 +86,18 @@ class User(UserMixin, db.Model):
       .group_by(Post)
       .order_by(Post.timestamp.desc())
     )
+  
+  def get_reset_password_token(self, expires_in=600):
+    return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in},
+                      app.config['SECRET_KEY'], algorithm='HS256')
+  
+  @staticmethod
+  def verify_reset_password_token(token):
+    try:
+      id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+    except:
+      return
+    return db.session.get(User, id)
   
 class Post(db.Model):
   id: so.Mapped[int] = so.mapped_column(primary_key=True)
