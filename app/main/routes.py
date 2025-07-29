19c44b15d9f2cdcbcd07b3a507a4229ee030e76c
Lorenzo <asocialinfroverf@gmail.com>
2025-07-29 07:37:28 -0500

    RESTRUCTURE project: add main blueprint

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,18 +1,17 @@
-from flask import (render_template, flash, redirect, url_for, request, g)
-from app import app, db
-from app.forms import (LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm)
-from app.models import (User, Post)
-from app.email import send_password_reset_email
-from flask_login import (current_user, login_user, logout_user, login_required)
-import sqlalchemy as sa
-from urllib.parse import urlsplit
-from datetime import (datetime, timezone)
+from flask import (render_template, flash, redirect, url_for, request, g, current_app)
+from flask_login import (current_user, login_required)
 from flask_babel import _, get_locale
+import sqlalchemy as sa
 from langdetect import detect, LangDetectException
+from datetime import (datetime, timezone)
+from app import db
+from app.main import bp
+from app.main.forms import (EditProfileForm, EmptyForm, PostForm)
+from app.models import (User, Post)
 from app.translate import translate
 
 
-@app.before_request
+@bp.before_request
 def before_request():
   if current_user.is_authenticated:
     current_user.last_seen = datetime.now(timezone.utc)
@@ -20,8 +19,8 @@ def before_request():
   g.locale = str(get_locale())
 
 
-@app.route('/', methods=['GET','POST'])
-@app.route('/index', methods=['GET','POST'])
+@bp.route('/', methods=['GET','POST'])
+@bp.route('/index', methods=['GET','POST'])
 @login_required
 def index():
   form = PostForm()
@@ -37,20 +36,20 @@ def index():
     return redirect(url_for('index'))
 
   page = request.args.get('page', 1, type=int)
-  posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  posts = db.paginate(current_user.following_posts(), page=page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False)
   next_page = url_for('index', page=posts.next_num) if posts.has_next else None
   prev_page = url_for('index', page=posts.prev_num) if posts.has_prev else None
   return render_template('index.html', title=_('Home'), posts=posts.items, form=form, next_page=next_page, prev_page=prev_page)
 
 
 
-@app.route('/user/<username>')
+@bp.route('/user/<username>')
 @login_required
 def user(username):
   user = db.first_or_404(sa.select(User).where(User.username == username))
   page = request.args.get('page', 1, type=int)
   query = user.posts.select().order_by(Post.timestamp.desc())
-  posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  posts = db.paginate(query, page=page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False)
   next_page = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
   prev_page = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
 
@@ -58,7 +57,7 @@ def user(username):
   return render_template('user.html', user=user, posts=posts, form=form, next_page=next_page, prev_page=prev_page)
 
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
   form = EditProfileForm(current_user.username)
@@ -74,7 +73,7 @@ def edit_profile():
   return render_template('edit_profile.html', title=_('Edit Profile'), form=form)
 
 
-@app.route('/follow/<username>', methods=['POST'])
+@bp.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
   form = EmptyForm()
@@ -94,7 +93,7 @@ def follow(username):
     return redirect(url_for('index'))
   
 
-@app.route('/unfollow/<username>', methods=['POST'])
+@bp.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
   form = EmptyForm()
@@ -114,19 +113,19 @@ def unfollow(username):
     return redirect(url_for('index'))
   
 
-@app.route('/explore')
+@bp.route('/explore')
 @login_required
 def explore():
   page = request.args.get('page', 1, type=int)
   query = sa.select(Post).order_by(Post.timestamp.desc())
-  posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  posts = db.paginate(query, page=page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False)
   next_page = url_for('explore', page=posts.next_num) if posts.has_next else None
   prev_page = url_for('explore', page=posts.prev_num) if posts.has_prev else None
   return render_template('index.html', title=_('Explore'), posts=posts.items, next_page=next_page, prev_page=prev_page)
 
 
 
-@app.route('/translate', methods=['POST'])
+@bp.route('/translate', methods=['POST'])
 @login_required
 def translate_text():
   data = request.get_json()
