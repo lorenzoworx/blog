e41033830589a7929e421dc4921eb33bfbf75a03
Lorenzo <asocialinfroverf@gmail.com>
2025-07-29 07:05:25 -0500

    RESTRUCTURE project: add auth blueprint

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -32,29 +32,7 @@ class RegistrationForm(FlaskForm):
       raise ValidationError(_('This email is already in use. Please try another.'))
     
 
-class EditProfileForm(FlaskForm):
-  username = StringField(_l('Username'), validators=[DataRequired()])
-  bio = TextAreaField(_l('Bio'), validators=[Length(min=0, max=140)])
-  submit = SubmitField(_l('Submit'))
 
-  def __init__(self, original_username, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.original_username = original_username
-
-  def validate_username(self, username):
-    if username.data != self.original_username:
-      user = db.session.scalar(sa.select(User).where(User.username == username.data))
-      if user is not None:
-        raise ValidationError(_('Apologies, that username is already in use. Please use a different username'))
-      
-
-class EmptyForm(FlaskForm):
-  submit = SubmitField('Submit')
-
-
-class PostForm(FlaskForm):
-  post = TextAreaField(_l("What's on your mind?"), validators=[DataRequired(), Length(min=1, max=140)])
-  submit = SubmitField(_l('Submit'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
