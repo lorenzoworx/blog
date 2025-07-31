8887998a0b3d828179ae4105f5071547e654361c
Lorenzo <asocialinfroverf@gmail.com>
2025-07-31 00:57:59 -0500

    BUG fix: pass appropriate app instance

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login, app
+from app import db, login
 from time import time
 import jwt
 from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from flask import current_app
 
 @login.user_loader
 def load_user(id):
@@ -89,12 +90,12 @@ class User(UserMixin, db.Model):
   
   def get_reset_password_token(self, expires_in=600):
     return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in},
-                      app.config['SECRET_KEY'], algorithm='HS256')
+                      current_app.config['SECRET_KEY'], algorithm='HS256')
   
   @staticmethod
   def verify_reset_password_token(token):
     try:
-      id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+      id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
     except:
       return
     return db.session.get(User, id)
