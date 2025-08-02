3cbdb2c10accbe03bc44cb5582aabecc07a2a57b
Lorenzo <asocialinfroverf@gmail.com>
2025-08-02 09:23:55 -0500

    ADD project environment file

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-aint-none-of-your-business'
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
